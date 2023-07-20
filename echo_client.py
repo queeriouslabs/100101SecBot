c268768ce6f3792b50d39cfa4be2ce5a5398e312
marcidy <marcidy@gmail.com>
2023-07-19 21:32:09 -0700

    echo_client.py fills role of test_echo_connect.py

--- echo_client.py
+++ echo_client.py
@@ -4,27 +4,21 @@ from app import create_app
 
 
 async def test_echo():
-    # sock = "./test_sock"
-    # reader, writer = await asyncio.open_unix_connection(sock)
     name = "echo_client"
     app = create_app(name)
-    await app.connect("echo")
-    reader, _ = app.connections['echo']
+    # await app.connect("echo")
+    # reader, _ = app.connections['echo']
 
     for x in range(0, 100):
         req = {
-            "source_id": "echo",
-            "target_id": "echo",
+            "source_id": name,
+            "target_id": 'echo',
             "permissions": [
                 {
                     "perm": "/open/the/door",
-                    "context": {"what": x}
+                    "context": {"number": x}
                 }]}
-        await app.request(req)
-
-        print(await reader.readline())
-
-    await app.disconnect("echo")
+        print(await app.request("echo", req))
 
 
 if __name__ == "__main__":
