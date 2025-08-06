86787fd864534f674fba756bc3c7d3829c2f1ae1
marcidy <marcidy@gmail.com>
2025-08-06 14:59:15 -0700

    first pass at a cli unlock service

---
+++ services/unlock.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+
+from secbot.comms import create_comms
+
+request = {
+    "source_id": "unlock_cli",
+    "target_id": "front_door_latch",
+    "permissions": [{
+        "perm": "/open",
+        "grant": True}]
+}
+
+
+async def cli_unlock(config, req):
+    name = "cli_unlock"
+    comms = create_comms(name, config)
+
+    user = os.getlogin()
+    comms.logger.info(f"cli unlock by {user}")
+
+    target_id = req["target_id"]
+    await comms.request(target_id, req)
+
+
+if __name__ == "__main__":
+    config = None
+    if os.environ.get("QUEERIOUSLABS_ENV", None) == 'PROD':
+        from settings import ProdConfig as config
+    else:
+        from settings import Config as config
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(cli_unlock(config, request))
